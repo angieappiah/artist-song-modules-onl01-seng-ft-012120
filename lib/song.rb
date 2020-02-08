b0ccf447c56df2451b1cbfd5c3eb43d3e4f35d6a
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-08 03:30:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,17 +4,13 @@ class Song
   attr_accessor :name
   attr_reader :artist
   
+  
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
 
- @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
+ 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
