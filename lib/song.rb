0bfca40eee55d7f61a5666bd68231453961fba61
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-08 03:27:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song
-
+  attr_accessor :name
+  attr_reader :artist
+  
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
-
 
  @@songs = []
 
